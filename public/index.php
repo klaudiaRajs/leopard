01ddd5690ee63f43efb9669cb570753b23307dec
klaudiaRajs <klaudia.rajs@gmail.com>
2019-04-22 22:42:27 +0100

    Stable version - routing

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use MyApp\Analyzer\Rules;
+use MyApp\Config\Config;
 use MyApp\Controller\FileAnalyzer;
 use MyApp\Statistics\StatKeeper;
 use MyApp\View\ViewRenderer;
@@ -15,7 +16,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
-
+set_time_limit(60);
 $app->register(new FormServiceProvider());
 $app->register(new Silex\Provider\LocaleServiceProvider());
 $app->register(new Silex\Provider\ValidatorServiceProvider());
@@ -26,14 +27,16 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../views',
 ));
 
-$app['asset.host'] = 'http://leopardslim.com/';
+$app['asset.host'] = Config::URL;
 
+//route for adding files for analysis
 $app->match('/addFile', function(Request $request) use ($app){
     $data = array(
         'file' => 'Please, provide a file to analyze',
         'introducedProblems' => 'Please provide number of introduced problems',
     );
 
+    //gets automatically build form
     $form = $app['form.factory']->createBuilder(FormType::class, $data)
         ->setAction($app['url_generator']->generate('uploadFile'))
         ->setMethod('POST')
@@ -56,7 +59,7 @@ $app->match('/addFile', function(Request $request) use ($app){
     return $app['twig']->render('index.twig', array('form' => $form->createView()));
 })->bind('addFile');
 
-
+//redirect from root to /addFile
 $app->get('/', function() use ($app){
     return $app->redirect('addFile');
 });
@@ -65,11 +68,12 @@ $app->get('uploadFile', function(Request $request) use ($app){
     $files = $request->files->get('form');
     $fileAnalyzer = new FileAnalyzer();
 
+    //handling of missing file
     if (!$files) {
         $result = "Please, provide files using this site: <a href=\"" . $app['asset.host'] . "addFile\">Add File</a>";
     }
 
-    $result = $fileAnalyzer->analyzeUpload("", 1, new StatKeeper(), $result);
+    $result = $fileAnalyzer->analyzeUpload("", 1, $result);
     return $result;
 });
 
@@ -82,52 +86,84 @@ $app->post('/uploadFile', function(Request $request) use ($app){
     } else {
         $result = '';
 
-        foreach ($files as $file) {
-            if (is_array($file)) {
-                $file = $file['file'];
-            }
-            $directory = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "upload" . DIRECTORY_SEPARATOR;
-            //        $fileName = count(scandir($directory)) . $file->getClientOriginalName();
-            $fileName = $file->getClientOriginalName();
-            try {
-                $file->move($directory, $fileName);
-            }catch(\Exception $e) {
-                echo $e->getMessage();
-            }
-        }
+        uploadFiles($files);
 
+        //Gets data from form
         $requestFields = $request->get('form');
 
+        //Analyzes contents of files
         /** @var FileAnalyzer $fileAnalyzer */
         $result = processRequest($files, $requestFields, $result, $fileAnalyzer);
+        //Adds summary of analysis
         $result .= $fileAnalyzer->analyzeResults();
     }
 
-    $result = $fileAnalyzer->analyzeUpload("", 1, new StatKeeper(), $result);
+    //Required for additional formatting
+    $result = $fileAnalyzer->analyzeUpload("", 1, $result);
 
     return $result;
 })->bind('uploadFile');
 
+//Function saves files uploaded in the form
+function uploadFiles($files){
+    foreach ($files as $file) {
+        if (is_array($file)) {
+            $file = $file['file'];
+        }
+        $directory = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "upload" . DIRECTORY_SEPARATOR;
+        $fileName = $file->getClientOriginalName();
+        try {
+            $file->move($directory, $fileName);
+        }catch(\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
+
+$app->get('/testRun', function() use ($app){
+    //Provides data for analysis in test run
+    $files = [['file' => 'FunctionSimilarityAnalyser.php', 'convention' => 'camelCase', 'introducedProblems' => 25], ['file' => 'Books.php', 'convention' => 'camelCase', 'introducedProblems' => 6]];
+    $fileAnalyzer = new FileAnalyzer();
+    $result = '';
+    //Analyzes contents of files
+    /** @var FileAnalyzer $fileAnalyzer */
+    $result = processRequestTestRun($files, $result, $fileAnalyzer);
+    //Adds summary of analysis
+    $result .= $fileAnalyzer->analyzeResults();
+    //Required for additional formatting
+    $result = $fileAnalyzer->analyzeUpload("", 1, $result);
+    return $result;
+});
+
+//Renders form for adding files for analysis
 $app->get('/addFile', function() use ($app){
     return ViewRenderer::render('FileUploader');
 });
 
+//Handles exceptions
 $app->error(function(\Exception $e, Request $request, $code){
     echo 'We are sorry, but something went terribly wrong.<br/>';
     echo 'Code: ' . $code . '<br/>';
     exit;
-    //    echo '<pre>';
-    //    print_r($e);
 });
 
-function processRequest($files, $requestFields, $result, FileAnalyzer $fileAnalyzer){
+//Provides separate method for analyzing files for test runs. Simpler than imitating fields from the form.
+function processRequestTestRun($files, $result, FileAnalyzer $fileAnalyzer){
+    foreach ($files as $file) {
+        Rules::setNamingConvention($file['convention']);
+        $result .= $fileAnalyzer->analyzeUpload($file['file'], $file['introducedProblems']);
+    }
+    $fileAnalyzer->statResultFilePath = StatKeeper::saveProgress();
+    return $result;
+}
 
-    $statKeeper = new StatKeeper();
+//Processes multiple files
+function processRequest($files, $requestFields, $result, FileAnalyzer $fileAnalyzer){
     if (isset($requestFields['convention'])) {
         Rules::setNamingConvention($requestFields['convention']);
         if (isset($files['file'])) {
             $fileName = $files['file']->getClientOriginalName();
-            $result .= $fileAnalyzer->analyzeUpload($fileName, $requestFields['introducedProblems'], $statKeeper);
+            $result .= $fileAnalyzer->analyzeUpload($fileName, $requestFields['introducedProblems']);
         }
     }
 
@@ -136,14 +172,14 @@ function processRequest($files, $requestFields, $result, FileAnalyzer $fileAnaly
             if (is_int($key)) {
                 Rules::setNamingConvention($formField['convention']);
                 $fileName = $files[$key]['file']->getClientOriginalName();
-                $result .= $fileAnalyzer->analyzeUpload($fileName, $formField['introducedProblems'], $statKeeper);
+                $result .= $fileAnalyzer->analyzeUpload($fileName, $formField['introducedProblems']);
             }
         }
     } else {
         $result .= "File not found";
     }
 
-    $fileAnalyzer->statResultFilePath = $statKeeper->saveProgress();
+    $fileAnalyzer->statResultFilePath = StatKeeper::saveProgress();
     return $result;
 }
 
